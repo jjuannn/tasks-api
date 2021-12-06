c7c8a80074440802e68f452ee0a5ba2e98ac7684
juan <juancraft12yt@gmail.com>
2021-12-05 22:02:57 -0300

    create task entity

---
+++ server/src/tasks/entity/tasks.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn, Table } from 'typeorm';
+
+@Entity({ name: 'tasks' })
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column({ default: false })
+  hasHighPriority: boolean;
+}
