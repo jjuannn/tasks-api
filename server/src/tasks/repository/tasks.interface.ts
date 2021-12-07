f93a899dc6e024ff6428a7545ef82bd91e853658
juan <juancraft12yt@gmail.com>
2021-12-06 23:27:05 -0300

    update repository

--- server/src/tasks/repository/tasks.interface.ts
+++ server/src/tasks/repository/tasks.interface.ts
@@ -4,9 +4,9 @@ import { Task } from '../entity/tasks.entity';
 export const ITASKS_REPOSTORY = 'ITASKS_REPOSTORY';
 
 export interface ITasksRepository {
-  getAllTasks(): Promise<TasksDTO[]>;
-  // getTask(id: number): Promise<TasksDTO>;
+  getAllTasks(): Promise<Task[]>;
+  getTask(id: number): Promise<Task>;
   createTask(task: Task): Promise<Task>;
-  //   deleteTask(): boolean;
-  //   updateTask(): boolean;
+  deleteTask(id: number): Promise<Task[]>;
+  updateTask(task: Task): Promise<Task>;
 }
--- server/src/tasks/repository/tasks.repository.ts
+++ server/src/tasks/repository/tasks.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { TasksDTO } from '../dto/tasks.dto';
@@ -12,13 +12,37 @@ export class TasksRepository implements ITasksRepository {
     private tasksRepository: Repository<Task>,
   ) {}
 
-  async getAllTasks(): Promise<TasksDTO[]> {
+  async getAllTasks(): Promise<Task[]> {
     const tasks = await this.tasksRepository.query('SELECT * FROM tasks');
+
+    if (!tasks) {
+      throw new NotFoundException('Cannot find tasks');
+    }
+
     return tasks;
+  }
+
+  async getTask(id: number): Promise<Task> {
+    const task = await this.tasksRepository.findOne({ where: { id } });
+
+    if (!task) {
+      throw new NotFoundException(`Cannot find task with id ${id}`);
+    }
+    return task;
   }
 
   async createTask(task: Task): Promise<Task> {
     const savedTask = await this.tasksRepository.save(task);
     return savedTask;
+  }
+
+  async updateTask(task: Task): Promise<Task> {
+    await this.tasksRepository.update(task.id, task);
+    return await this.getTask(task.id);
+  }
+
+  async deleteTask(id: number): Promise<Task[]> {
+    await this.tasksRepository.delete({ id });
+    return await this.getAllTasks();
   }
 }
