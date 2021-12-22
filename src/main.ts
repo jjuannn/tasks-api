e49e5430b08c58e9b3d95152e222efb0d3a636c4
juan <juancraft12yt@gmail.com>
2021-12-22 12:44:37 -0300

    change default port

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({ origin: '*' });
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
