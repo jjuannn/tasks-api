c2571232886f00c340d6ce34e510a5d7451514f9
Juan Avero <juan.avero@alkemy.org>
2023-07-05 15:14:28 -0300

    Modify API layers to implement new domain classes

--- src/tasks/controller/tasks.interface.ts
+++ src/tasks/controller/tasks.interface.ts
@@ -1,12 +1,14 @@
-import { TasksDTO } from '../dto/tasks.dto';
+import { CreateTaskDTO } from '../dto/create-task-dto';
+import { UpdateTaskDTO } from '../dto/update-task.dto';
 import { Task } from '../entity/tasks.entity';
+import { PriorityTypes } from '../enum/priority.enum';
 
 export interface ITasksController {
   getAllTasks(): Promise<Task[]>;
   getTask(id: string): Promise<Task>;
-  createTask(body: TasksDTO): Promise<Task>;
+  createTask(body: CreateTaskDTO): Promise<Task>;
   getByTitle(title: string): Promise<Task[]>;
-  getByPriority(priority: string): Promise<Task[]>;
-  deleteTask(id: string): Promise<Object>;
-  updateTask(body: TasksDTO, id: string): Promise<Task>;
+  getByPriority(priority: PriorityTypes): Promise<Task[]>;
+  deleteTask(id: string): Promise<{ success: boolean }>;
+  updateTask(body: UpdateTaskDTO, id: string): Promise<Task>;
 }
--- src/tasks/repository/tasks.interface.ts
+++ src/tasks/repository/tasks.interface.ts
@@ -1,4 +1,5 @@
-import { TasksDTO } from '../dto/tasks.dto';
+import { CreateTask } from '../entity/domain/create-task.entity';
+import { UpdateTask } from '../entity/domain/update-task.entity';
 import { Task } from '../entity/tasks.entity';
 
 export const ITASKS_REPOSTORY = 'ITASKS_REPOSTORY';
@@ -6,9 +7,9 @@ export const ITASKS_REPOSTORY = 'ITASKS_REPOSTORY';
 export interface ITasksRepository {
   getAllTasks(): Promise<Task[]>;
   getTask(id: number): Promise<Task>;
-  createTask(task: Task): Promise<Task>;
+  createTask(task: CreateTask): Promise<Task>;
   getByTitle(title: string): Promise<Task[]>;
   getByPriority(priority: string): Promise<Task[]>;
-  deleteTask(id: number): Promise<Object>;
-  updateTask(task: Task): Promise<Task>;
+  deleteTask(id: number): Promise<{ success: boolean }>;
+  updateTask(task: UpdateTask): Promise<Task>;
 }
